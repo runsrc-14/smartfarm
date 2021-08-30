70cd410262a02f86c1f94f7314c1a0ae57592de2
rundev <ssi737423@gmail.com>
2021-08-30 21:52:16 +0700

    pass please

--- src/models/waether.model.js
+++ src/models/waether.model.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, Sequelize) => {
     const Waether = sequelize.define("weather", {
-        date: {
+        Date: {
             type: Sequelize.DATE,
             default: Date.now
         },
