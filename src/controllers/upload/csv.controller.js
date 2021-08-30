273f6b996db15dc0a5d42f62f27f2b1d730593e6
rundev <ssi737423@gmail.com>
2021-08-30 22:04:26 +0700

    pass please

--- src/controllers/upload/csv.controller.js
+++ src/controllers/upload/csv.controller.js
@@ -4,14 +4,14 @@ const Tutorial = db.weathers;
 const fs = require("fs");
 const csv = require("fast-csv");
 const CsvParser = require("json2csv").Parser;
-
+const CsvData = [];
 const upload = async (req, res) => {
   try {
     if (req.file == undefined) {
       return res.status(400).send("Please upload a CSV file!");
     }
 
-    let tutorials = [];
+    // let tutorials = [];
     let path = __basedir + "/resources/static/assets/uploads/" + req.file.filename;
 
     fs.createReadStream(path)
@@ -20,23 +20,24 @@ const upload = async (req, res) => {
         throw error.message;
       })
       .on("data", (row) => {
-        tutorials.push(row);
+        CsvData.push(row);
       })
       .on("end", () => {
+        res.send(CsvData);
         // console.log(tutorials);
-        Tutorial.bulkCreate(tutorials)
-          .then(() => {
-            res.status(200).send({
-              message:
-                "Uploaded the file successfully: " + req.file.originalname,
-            });
-          })
-          .catch((error) => {
-            res.status(500).send({
-              message: "Fail to import data into database!",
-              error: error.message,
-            });
-          });
+        // Tutorial.bulkCreate(tutorials)
+        //   .then(() => {
+        //     res.status(200).send({
+        //       message:
+        //         "Uploaded the file successfully: " + req.file.originalname,
+        //     });
+        //   })
+        //   .catch((error) => {
+        //     res.status(500).send({
+        //       message: "Fail to import data into database!",
+        //       error: error.message,
+        //     });
+        //   });
       });
   } catch (error) {
     console.log(error);
@@ -47,16 +48,17 @@ const upload = async (req, res) => {
 };
 
 const getTutorials = (req, res) => {
-  Tutorial.findAll()
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving tutorials.",
-      });
-    });
+  res.send(CsvData);
+  // Tutorial.findAll()
+  //   .then((data) => {
+  //     res.send(data);
+  //   })
+  //   .catch((err) => {
+  //     res.status(500).send({
+  //       message:
+  //         err.message || "Some error occurred while retrieving tutorials.",
+  //     });
+  //   });
 };
 
 const download = (req, res) => {
