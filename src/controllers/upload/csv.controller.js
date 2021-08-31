13bf449349a4ed30d70d4a34d7c0f65357bf1c09
rundev <ssi737423@gmail.com>
2021-08-31 19:34:26 +0700

    passplease

--- src/controllers/upload/csv.controller.js
+++ src/controllers/upload/csv.controller.js
@@ -62,8 +62,7 @@ const getTutorials = (req, res) => {
 };
 const DeleteAPI = (req,res) =>{
   CsvData.splice(0)
-  res.status(200).end(csvData);
-  // res.send(CsvData);
+  res.send(CsvData);
 };
 const download = (req, res) => {
   Tutorial.findAll().then((objs) => {
