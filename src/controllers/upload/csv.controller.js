644734617123769f371dfb2e3e2f06cdee53381c
rundev <ssi737423@gmail.com>
2021-10-05 01:42:33 +0700

    dev

--- src/controllers/upload/csv.controller.js
+++ src/controllers/upload/csv.controller.js
@@ -4,14 +4,14 @@ const weathers = db.weathers
 const fs = require("fs");
 const csv = require("fast-csv");
 const CsvParser = require("json2csv").Parser;
-let fileuploads = [];
+// let fileuploads = [];
 const Op = db.Sequelize.Op;
 const upload = async (req, res) => {
   try {
     if (req.file == undefined) {
       return res.status(400).send("Please upload a CSV file!");
     }
-    // let fileuploads = [];
+    let fileuploads = [];
     let path = __basedir + "/resources/static/assets/uploads/" + req.file.filename;
 
     fs.createReadStream(path)
@@ -23,43 +23,40 @@ const upload = async (req, res) => {
         fileuploads.push(row);
       })
       .on("end", () => {
-       res.send({ fileuploads })
-        // for (i = 0; i < fileuploads.length; i++) {
-        //   weathers.create({
-        //     Date: fileuploads[i].Date,
-        //     MaxTemp: fileuploads[i].MaxTemp,
-        //     MinTemp: fileuploads[i].MinTemp,
-        //     Rainfall: fileuploads[i].Rainfall,
-        //     Evaporation: fileuploads[i].Evaporation,
-        //     Sunshine: fileuploads[i].Sunshine,
-        //     WindGustSpeed: fileuploads[i].WindGustSpeed,
-        //     WindSpeed9am: fileuploads[i].WindSpeed9am,
-        //     WindSpeed3pm: fileuploads[i].WindSpeed3pm,
-        //     Humidity9am: fileuploads[i].Humidity9am,
-        //     Humidity3pm: fileuploads[i].Humidity3pm,
-        //     Pressure9am: fileuploads[i].Pressure9am,
-        //     Pressure3pm: fileuploads[i].Pressure3pm,
-        //     Cloud9am: fileuploads[i].Cloud9am,
-        //     Cloud3pm: fileuploads[i].Cloud3pm,
-        //     Temp9am: fileuploads[i].Temp9am,
-        //     Temp3pm: fileuploads[i].Temp3pm,
-        //     RISK_MM: fileuploads[i].RISK_MM,
-        //     userId: req.params.id
-        //   }).then(() => {
-        //     res.status(200).send({
-        //       message:
-        //         "Uploaded the file successfully: " + req.file.originalname + req.params.id,
-        //     });
-        //   })
-
-        //     // })
-        //     .catch((error) => {
-        //       res.status(500).send({
-        //         message: "Fail to import data into database!",
-        //         error: error.message,
-        //       });
-        //     });
-        // }
+        //  res.send({ fileuploads })
+        for (i = 0; i < fileuploads.length; i++) {
+          weathers.create({
+            Date: fileuploads[i].Date,
+            MaxTemp: fileuploads[i].MaxTemp,
+            MinTemp: fileuploads[i].MinTemp,
+            Rainfall: fileuploads[i].Rainfall,
+            Evaporation: fileuploads[i].Evaporation,
+            Sunshine: fileuploads[i].Sunshine,
+            WindGustSpeed: fileuploads[i].WindGustSpeed,
+            WindSpeed9am: fileuploads[i].WindSpeed9am,
+            WindSpeed3pm: fileuploads[i].WindSpeed3pm,
+            Humidity9am: fileuploads[i].Humidity9am,
+            Humidity3pm: fileuploads[i].Humidity3pm,
+            Pressure9am: fileuploads[i].Pressure9am,
+            Pressure3pm: fileuploads[i].Pressure3pm,
+            Cloud9am: fileuploads[i].Cloud9am,
+            Cloud3pm: fileuploads[i].Cloud3pm,
+            Temp9am: fileuploads[i].Temp9am,
+            Temp3pm: fileuploads[i].Temp3pm,
+            RISK_MM: fileuploads[i].RISK_MM,
+            userId: req.params.id
+          }).then(() => {
+            res.status(200).send({
+              message:
+                "Uploaded the file successfully: " + req.file.originalname + req.params.id,
+            });
+          }).catch((error) => {
+              res.status(500).send({
+                message: "Fail to import data into database!",
+                error: error.message,
+              });
+            });
+        }
         // fileuploads = []
       });
   } catch (error) {
@@ -96,12 +93,13 @@ const Getcsv = (req, res) => {
     });
 };
 const GetAllcsv = (req, res) => {
-  // weathers.findAll().then((data) => {
-  //   if (data) {
-  //     res.send(data)
-  //   }
-  // })
-  res.send(fileuploads);
+  // res.send(fileuploads);
+  weathers.findAll().then((data) => {
+    if (data) {
+      res.send(data)
+    }
+  })
+
 }
 const Deletecsv = (req, res) => {
   // fileuploads.splice(0)
@@ -129,10 +127,16 @@ const Deletecsv = (req, res) => {
     });
 }
 const DeleteAllcsv = (req, res) => {
+  // fileuploads.splice(0)
+  // return res.send(fileuploads).send({ message: 'ลบข้อมูลแล้วจ้ะ' });
   weathers.destroy({
     where: {},
     truncate: false,
-  })
+  }).then((result) => {
+    res.send(result)
+  }).catch((err) => {
+    res.send(err)
+  });
 }
 module.exports = {
   GetAllcsv,
